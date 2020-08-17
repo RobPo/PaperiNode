7abba51d7710c5e75d9929c35f994beb9712542f
Robert Poser <21104467+RobPo@users.noreply.github.com>
2020-08-17 19:23:30 +0200

    Update 02_HelloWorld.ino
    Comments added

--- examples/02_HelloWorld/02_HelloWorld.ino
+++ examples/02_HelloWorld/02_HelloWorld.ino
@@ -1,16 +1,17 @@
 #include "paperinode.h"
 #include "PL_microEPD44.h"
 
-PL_microEPD epd(EPD_CS, EPD_RST, EPD_BUSY);    //Initialize the EPD.
+PL_microEPD epd(EPD_CS, EPD_RST, EPD_BUSY);    	//Initialize the EPD.
 
 void setup(void) {  
-  analogReference(EXTERNAL);                   // use AREF for reference voltage
-  SPI.begin();                                 // Initialize the SPI port
+  analogReference(EXTERNAL);                   	// use AREF for reference voltage
+  SPI.begin();                                 	// Initialize the SPI port
   SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
 
-  epd.begin();                                 // Turn ON & initialize 1.1" EPD
-  epd.printText("Hello World!", 35, 20, 1);     
-  epd.update();                     
+  epd.begin();                                 	// Turn ON & initialize 1.1" EPD
+  // printText(String text, int x, int y, int size);
+  epd.printText("Hello World!", 35, 20, 1);    	// Add text to framebuffer
+  epd.update();                     			// Send framebuffer to EPD and trigger update
 }
 
 void loop(){ 
